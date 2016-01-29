993e8b621396430fc62154f62b430d1d9d57e267
Haomeng, Wang <whaom@cn.ibm.com>
2016-01-29 07:18:39 +0000

    fix typo of agent.py

--- ironic/drivers/agent.py
+++ ironic/drivers/agent.py
@@ -64,7 +64,7 @@ class AgentAndIPMIToolDriver(base.BaseDriver):
         self.raid = agent.AgentRAID()
         self.inspect = inspector.Inspector.create_if_enabled(
             'AgentAndIPMIToolDriver')
-        self.clone = agent.AgetnClone()
+        self.clone = agent.AgentClone()
 
 
 class AgentAndIPMINativeDriver(base.BaseDriver):
